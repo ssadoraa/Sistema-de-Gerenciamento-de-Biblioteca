85750aaa684e06913aa5416918d977e8f379b027
BernardoMendess <bernardo_mendes@id.uff.br>
2024-01-08 23:45:01 -0300

    iniciando o projeto

---
+++ src/main/java/com/sgb/biblioteca/BibliotecaApplication.java
@@ -0,0 +1,13 @@
+package com.sgb.biblioteca;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BibliotecaApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(BibliotecaApplication.class, args);
+	}
+
+}
