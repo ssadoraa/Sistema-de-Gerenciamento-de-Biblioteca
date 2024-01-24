e4c6b1bd4d3bdb15e4fa319cd6e1fea42fee1ed4
ssadoraa <isadoraverbicario@hotmail.com>
2024-01-24 14:17:17 -0300

    ajuste URL

--- src/main/java/com/sgb/biblioteca/controller/normal/LivroController.java
+++ src/main/java/com/sgb/biblioteca/controller/normal/LivroController.java
@@ -12,9 +12,12 @@ import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+
 
 @Controller 
 @AllArgsConstructor
+@RequestMapping("/livro")
 public class LivroController {
 
     private LivroService livroService;
